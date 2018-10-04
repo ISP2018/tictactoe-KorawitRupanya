6f33def0bf0035febe7d03ec2b44fbcc1abd4dcb
KorawitRupanya <smart1micky268@gmail.com>
2018-10-04 15:44:33 +0700

    do assignment

--- src/tictactoe/GameController.java
+++ src/tictactoe/GameController.java
@@ -1,5 +1,6 @@
 package tictactoe;
 
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -9,10 +10,8 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 
 /**
- * UI controller for the JavaFX interface to tic-tac-toe game.
- * This class handles user input events.
- * 
- * @author jim
+ * UI controller for the JavaFX interface to tic-tac-toe game. This class
+ * handles user input events.
  */
 public class GameController {
 	@FXML
@@ -21,14 +20,13 @@ public class GameController {
 	private Pane centerPane;
 	@FXML
 	private Button newGameButton;
-	
+
 	private TicTacToeGame game;
-	
 
 	public GameController() {
 		// nothing to iniitialize yet.
 	}
-	
+
 	@FXML
 	public void initialize() {
 		game = new TicTacToeGame(4);
@@ -37,33 +35,35 @@ public class GameController {
 		centerPane.getChildren().add(board);
 		centerPane.prefWidthProperty().bind(board.prefWidthProperty());
 		centerPane.prefHeightProperty().bind(board.prefHeightProperty());
-		
+
 		// listen to each square for mouse click
 		EventHandler<MouseEvent> mouseClicked = this::handleCellClicked;
 		board.getChildren().forEach(child -> child.setOnMouseClicked(mouseClicked));
 		// The "New Game" button action
-		newGameButton.setOnAction( this::handleNewGameEvent );
-		
+		newGameButton.setOnAction(this::handleNewGameEvent);
+
 		// Listen to TicTacToeGame for changes in status.
-		game.gameOver().addListener( (observable,oldValue,newValue)-> updateGameStatus());
-		
+		game.gameOver().addListener((observable, oldValue, newValue) -> updateGameStatus());
+
 		updateGameStatus();
 	}
-	
+
 	private void updateGameStatus() {
 		Player winner = game.winner();
-		if (winner != Player.NONE) topLabel.setText("Player "+winner+" wins!");
-		else if (game.isGameOver()) topLabel.setText("Draw. No winner.");
-		else topLabel.setText("Next Player: " + game.getNextPlayer());
-		
+		if (winner != Player.NONE)
+			topLabel.setText("Player " + winner + " wins!");
+		else if (game.isGameOver())
+			topLabel.setText("Draw. No winner.");
+		else
+			topLabel.setText("Next Player: " + game.getNextPlayer());
+
 	}
 
-	
 	/** Event handler for mouse clicks on game board. */
 	public void handleCellClicked(MouseEvent event) {
 		Object source = event.getSource();
 		if (source instanceof BoardSquare) {
-			BoardSquare cell = (BoardSquare)source;
+			BoardSquare cell = (BoardSquare) source;
 			int row = cell.getRow();
 			int col = cell.getColumn();
 			double size = cell.getHeight();
@@ -76,7 +76,7 @@ public class GameController {
 			updateGameStatus();
 		}
 	}
-	
+
 	/** Handler for button click to start a new game. */
 	public void handleNewGameEvent(ActionEvent event) {
 		game.startNewGame();
